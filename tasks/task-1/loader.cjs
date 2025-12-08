7a2019a79ddf87bcdb2924668d05ac0ba66c49fa
Dauren <babayev@kolesa.kz>
2025-12-08 23:48:15 +0500

    hw-1

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,13 @@
 const yaml = require("yaml");
 const fs = require("node:fs");
+
+function parseYamlFile(module, filename) {
+
+    const yamlText = fs.readFileSync(filename, 'utf8');
+
+    module.exports = yaml.parse(yamlText);
+}
+
+require.extensions['.yml'] = parseYamlFile
+
+require.extensions['.yaml'] = parseYamlFile
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,19 @@
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import yaml from "yaml";
+
+export async function load(url, context, nextLoad) {
+    if (url.endsWith('.yaml') || url.endsWith('.yml')) {
+        const filepath = fileURLToPath(url);
+        const fileContent = await fs.readFile(filepath, 'utf8');
+        const parsedContent = yaml.parse(fileContent);
+
+        return {
+            format: 'json',
+            shortCircuit: true,
+            source: JSON.stringify(parsedContent),
+        };
+    }
+
+    return nextLoad(url, context);
+}
