482bb7d63a987a01f059bd0fe6ae399f1cd6f702
Dauren <babayev@kolesa.kz>
2025-12-10 22:53:08 +0500

    hw-2 duplicate plugin

--- .yarn/plugins/plugin-duplicates-list.cjs
+++ .yarn/plugins/plugin-duplicates-list.cjs
@@ -3,5 +3,40 @@ module.exports = {
   factory: (require) => {
     const { BaseCommand } = require(`@yarnpkg/cli`);
     const { Configuration, Project, structUtils } = require("@yarnpkg/core");
+
+      class Command extends BaseCommand {
+          static paths = [[`duplicates`]];
+
+          async execute() {
+
+              const configuration = await Configuration.find(this.context.cwd, this.context.plugins);
+              const { project } = await Project.find(configuration, this.context.cwd);
+
+              await project.restoreInstallState();
+
+              const packageMap = new Map();
+
+              for (const pkg of project.storedPackages.values()) {
+
+                  if (!structUtils.isVirtualLocator(pkg)) {
+
+                      if (packageMap.has(pkg.name)) {
+
+                          const existingVersion = packageMap.get(pkg.name);
+
+                          this.context.stdout.write(pkg.name + " " + existingVersion + " " + pkg.version + "  ");
+                      }
+
+                      packageMap.set(pkg.name, pkg.version);
+                  }
+              }
+          }
+      }
+
+      return {
+          commands: [
+              Command,
+          ],
+      };
   },
 };
