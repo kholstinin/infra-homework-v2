d948ba43c97b63fec2bf1ef0a9eb96feed19242f
Artem <artemgumarov840@gmail.com>
2025-12-14 12:13:51 +0300

    task-1

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,16 @@
 const yaml = require("yaml");
 const fs = require("node:fs");
+
+function loadYaml(module, filename) {
+  const content = fs.readFileSync(filename, "utf8");
+  try {
+    const parsed = yaml.parse(content);
+    module.exports = parsed;
+  } catch (error) {
+    error.message = filename + ": " + error.message;
+    throw error;
+  }
+}
+
+require.extensions[".yml"] = loadYaml;
+require.extensions[".yaml"] = loadYaml;
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,39 @@
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import yaml from "yaml";
+
+export async function load(url, context, defaultLoad) {
+  if (url.endsWith(".yaml") || url.endsWith(".yml")) {
+    return addShortCircuitFlag(async () => {
+      const filename = fileURLToPath(url);
+      const content = await fs.readFile(filename, "utf8");
+      try {
+        const parsed = yaml.parse(content);
+        return {
+          format: "module",
+          source: `export default ${JSON.stringify(parsed)}`,
+        };
+      } catch (error) {
+        error.message = filename + ": " + error.message;
+        throw error;
+      }
+    });
+  }
+
+  return await defaultLoad(url, context);
+}
+
+export async function resolve(url, context, defaultResolve) {
+  return await defaultResolve(url, context);
+}
+
+async function addShortCircuitFlag(fn) {
+  const ret = await fn();
+
+  if (ret == null) return ret;
+
+  return {
+    ...ret,
+    shortCircuit: true,
+  };
+}
